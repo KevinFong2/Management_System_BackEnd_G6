00e2a7399813d82dfea254057f0c68b97bd74c4e
Kevin <kevinfong21@gmail.com>
2023-05-09 19:01:52 -0400

    added route to edit a employee

--- database/app.js
+++ database/app.js
@@ -23,10 +23,10 @@ app.get('/tasks', async (req, res) => {
       console.error(error);
       res.status(500).json({ message: 'Internal server error' });
     }
-  });
+});
 
-  //find single employee based on their id
-  app.put('/employees/:id', async (req, res) => {
+//find single employee based on their id
+app.put('/employees/:id', async (req, res) => {
     try {
       const employee = await Employees.findOne({ where: { id: req.params.id } });
       
@@ -46,10 +46,10 @@ app.get('/tasks', async (req, res) => {
       console.error(error);
       res.status(500).json({ message: 'Internal server error' });
     }
-  });
+});
 
-  //find single task based on their id
-  app.get('/tasks/:id', async (req, res) => {
+//find single task based on their id
+app.get('/tasks/:id', async (req, res) => {
     try {
       const task = await Tasks.findOne({
         where: { id: req.params.id },
@@ -68,4 +68,26 @@ app.get('/tasks', async (req, res) => {
       console.error(error);
       res.status(500).json({ message: 'Internal server error' });
     }
-  });
+});
+
+//edit employee
+app.put('/employees/:id', async (req, res) => {
+    try {
+      const employee = await Employees.findOne({ where: { id: req.params.id } });
+      
+      if (!employee) {
+        return res.status(404).send('Employee not found');
+      }
+  
+      const editedEmployee = await employee.update({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        department: req.body.department
+      });
+  
+      return res.status(200).json(editedEmployee);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+});
